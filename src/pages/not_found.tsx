a4fedd7286e13e3921d838647e03bc178893378e
cfleung <u3560667@connect.hku.hk>
2023-02-18 16:43:02 +0800

    feat[not-found-page]: added button to route to home page

--- src/pages/not_found.tsx
+++ src/pages/not_found.tsx
@@ -1,22 +1,33 @@
+import Button from '@components/Button'
 import { CSSProperties } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+  const navigate = useNavigate()
+
   const styles: Record<string, CSSProperties> = {
     container: {
       flex: 1,
       display: 'flex',
+      flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
     },
     text: {
       fontSize: 48,
       textAlign: 'center',
+      marginBottom: 16,
     },
+  }
+
+  const redirectMainPage = () => {
+    navigate('/')
   }
 
   return (
     <div style={styles.container}>
       <h1 style={styles.text}>Page not found</h1>
+      <Button onClick={redirectMainPage}>Home</Button>
     </div>
   )
 }
