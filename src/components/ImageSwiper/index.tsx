34f5e82c92e168d7f7fef4d59903797e5b6cdfc7
cfleung <u3560667@connect.hku.hk>
2023-02-22 17:31:13 +0800

    feat[image-swiper]: added zoom in function

--- src/components/ImageSwiper/index.tsx
+++ src/components/ImageSwiper/index.tsx
@@ -21,11 +21,10 @@ const ImageSwiper = ({
   width: number | string
   height: number | string
 }) => {
-  const [slideRefs] = useState<Array<RefObject<HTMLImageElement>>>(
-    children.map(() => createRef<HTMLImageElement>()),
-  )
   const [imageIndex, setImageIndex] = useState<number>(0)
   const containerRef = useRef<HTMLDivElement>(null)
+  const [zoomed, setZoomed] = useState<boolean>(false)
+  const [imageSource, setImageSource] = useState<string>('')
 
   useEffect(() => {
     const listeningFunc = (e: any) => {
@@ -50,7 +49,7 @@ const ImageSwiper = ({
   }
 
   const handleNextImageClick = () => {
-    if (imageIndex + 1 < slideRefs.length) scrollToIndex(imageIndex + 1)
+    if (imageIndex + 1 < children.length) scrollToIndex(imageIndex + 1)
   }
 
   return (
@@ -58,7 +57,6 @@ const ImageSwiper = ({
       <div className="slide-container" ref={containerRef}>
         {children.map((child, index) => {
           return cloneElement(child, {
-            imageRef: slideRefs[index],
             key: `image-${index}`,
           })
         })}
@@ -76,28 +74,61 @@ const ImageSwiper = ({
           styles={{ width: 50, height: 50 }}
           icon="images/icons/right-arrow.png"
           onClick={handleNextImageClick}
-          disabled={imageIndex === slideRefs.length - 1}
+          disabled={imageIndex === children.length - 1}
         />
       </div>
       <div className="nagivate-buttons-container">
-        {slideRefs.map((_ref, index) => (
+        {children.map((_, childIndex) => (
           <IconButton
-            key={`navgivate-button-${index}`}
-            onClick={() =>
-              // setImageIndex(() => {
-              //   // slideRefs[index].current?.scrollIntoView()
-              //   scrollToIndex(index)
-              //   return index
-              // })
-              scrollToIndex(index)
-            }
+            key={`navgivate-button-${childIndex}`}
+            onClick={() => scrollToIndex(childIndex)}
             styles={{
               width: 20,
               height: 20,
-              ...(index === imageIndex && { opacity: 1 }),
+              ...(childIndex === imageIndex && { opacity: 1 }),
             }}
           />
         ))}
+      </div>
+      <div className="zoom-in-button-container">
+        <IconButton
+          onClick={() => {
+            const src = children[imageIndex].props.src
+            if (src && typeof src === 'string') {
+              setImageSource(src)
+              setZoomed(true)
+            }
+          }}
+          icon="images/icons/zoom-in.png"
+          styles={{
+            width: 50,
+            height: 50,
+          }}
+        />
+      </div>
+      <div
+        className="zoomed-container"
+        style={{ display: zoomed ? 'flex' : 'none' }}
+      >
+        <img
+          src={imageSource}
+          alt="zoomed"
+          className="zoomed-image"
+          loading="lazy"
+        />
+        <div
+          className="zoom-out-button-container"
+          style={{ display: zoomed ? 'block' : 'none' }}
+        >
+          <IconButton
+            icon="images/icons/zoom-out.png"
+            styles={{
+              width: 50,
+              height: 50,
+            }}
+            onClick={() => setZoomed(false)}
+          />
+        </div>
       </div>
     </div>
   )
