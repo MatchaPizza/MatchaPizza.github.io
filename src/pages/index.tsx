d603b5f99a66622675b8d09ba5aefb1ce31c6321
cfleung <u3560667@connect.hku.hk>
2024-05-19 21:45:34 +0800

    main page background uses lazy image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,13 @@ const MainPage = () => {
   return (
     <Fragment>
       <div className="introduction-container">
+        <LazyImage
+          src="/images/background.jpg"
+          hash="LA7Bf@%j-:xZ00RPM|WBxrMvM^RO"
+          alt="background"
+          width="100%"
+          height="100%"
+        />
         <div className="main-text-box">
           <h1 className="main-text">Hobby Page</h1>
           <div className="secondary-text">
